3cdba61284898a5b52edc30c84f10995603db7ba
guipupio <guipupio@gmail.com>
2019-05-26 22:17:57 -0300

    tentativas

--- EFC2_IA353_1s2019/questao_1.py
+++ EFC2_IA353_1s2019/questao_1.py
@@ -46,19 +46,40 @@ def get_calcula_media(results:list):
     return {'loss': loss/len(results), 'acc':acc/len(results)}
  
 
-def desenvolvimento_():
+def desenvolvimento_(num_epochs, units_camada_um, dropout_um, camada_dois, dropout_dois, gradient):
     X_train = x_train.reshape(60000,28,28,1)
     X_test = x_test.reshape(10000,28,28,1)
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dropout(0.2))
-    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(units_camada_um, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dropout(dropout_um))
+    model.add(tf.keras.layers.Dense(camada_dois, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dropout(dropout_dois))
+    
     model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
     
-    model.compile(optimizer=tf.train.GradientDescentOptimizer(0.17),
+    model.compile(optimizer=tf.train.GradientDescentOptimizer(gradient),
      loss='sparse_categorical_crossentropy',
      metrics=['accuracy'])
-    model.fit(X_train, y_train, epochs=7, use_multiprocessing=True)
+    model.fit(X_train, y_train, epochs=num_epochs, use_multiprocessing=True)
     return model.evaluate(X_test, y_test, use_multiprocessing=True)
+
+desenvolvimento_(num_epochs=11)
+
+num_file = 2
+n = 10
+for num_epoch in [10, 11, 12]:
+    for camada_um in [512, 800]:
+        for dropout_um in [0.2,0.3,0.4]:
+            for camada_dois in [32,64,128]:
+                for dropout_dois in [0.2,0.3,0.4]:
+                    for gradient in [0.17,0.18]:
+                        file = file = open(r"C:\Users\Gabriel\.pupio\Redes_neuraias\EFC2_IA353_1s2019\tentativa_erro_"+ str(num_file) +".txt","w+")
+                        file.writelines("Parametros: num_epoch=" + str(num_epoch) + "; camada_um="+ str(camada_um) + "; dropout_um="+ str(dropout_um) + "; camada_dois="+ str(camada_dois) + "; dropout_dois="+ str(dropout_dois) + "; gradient=" + str(gradient) + "\n[")
+                        for i in range(n):
+                            print("Iteracao: " + str(i+1) + "\nProgresso: " + str(int(100*(1+i)/n)) +"%" )
+                            file.writelines(str(desenvolvimento_(num_epochs=num_epoch, units_camada_um=camada_um, dropout_um=dropout_um, camada_dois=camada_dois, dropout_dois=dropout_dois, gradient=gradient))+",\n")
+                        
+                        file.writelines("]")
+                        file.close()
+                        num_file += 1
